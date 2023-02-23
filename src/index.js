70e390d8e3a0a9951639fce65f1113b269e3504f
gajendra <gk@11.com>
2023-02-23 15:52:55 +0530

    src added

--- src/index.js
+++ src/index.js
@@ -2,11 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "../node_modules/bootstrap/dist/css/bootstrap.css";
 import "../node_modules/bootstrap-icons/font/bootstrap-icons.css";
-import { BrowserRouter } from "react-router-dom";
 import "./index.css";
-import App from "./App";
+
 import Index from "./Project/Index";
-import BasicDetails from "./Project/BasicDetails";
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -16,9 +15,5 @@ root.render(
 </React.StrictMode>
 
 
-  // <BrowserRouter>
-  //   <React.StrictMode>
-  //     <App />
-  //   </React.StrictMode>
-  // </BrowserRouter>
+  
 );
